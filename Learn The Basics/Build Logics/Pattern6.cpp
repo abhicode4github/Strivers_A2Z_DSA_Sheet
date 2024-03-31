40963a2a933139c4bc9dec71ea47d5dd08a1b505
Abhishek jha <abhishekjhagzb2001@gmail.com>
2024-03-31 18:42:42 +0530

    Create Pattern6.cpp

---
+++ Learn The Basics/Build Logics/Pattern6.cpp
@@ -0,0 +1,24 @@
+Problem statement
+Aryan and his friends are very fond of the pattern. For a given integer ‘N’, they want to make the Reverse N-Number Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+1 2 3
+1 2
+1
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+void nNumberTriangle(int n) {
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i+1;j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
