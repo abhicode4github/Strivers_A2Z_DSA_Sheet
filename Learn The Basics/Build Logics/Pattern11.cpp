ca0e5543840ee7fe688c09f692969572dc507f8d
Abhishek jha <abhishekjhagzb2001@gmail.com>
2024-04-04 17:33:53 +0530

    Create Pattern11.cpp

---
+++ Learn The Basics/Build Logics/Pattern11.cpp
@@ -0,0 +1,33 @@
+Problem statement
+Aryan and his friends are very fond of the pattern. For a given integer ‘N’, they want to make the N-Binary Number Triangle.
+
+You are required to print the pattern as shown in the examples below.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+1
+0 1
+1 0 1
+
+----------------------------------------------------------------------------------------------------------------------------
+
+  void nBinaryTriangle(int n) {
+    int start;
+
+    for(int i = 0; i < n; i++) {
+        if (i % 2 == 0)
+            start = 1;
+        else
+            start = 0;
+
+        for(int j = 0; j <= i; j++) {
+            cout << start<<" ";
+            start = 1 - start;
+        }
+
+        cout << endl;
+    }
+}
